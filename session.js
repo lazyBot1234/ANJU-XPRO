1488d9546b9a9eded01d71294ecf1ce088f2a5dc
lazyBot1234 <dilishad170@gmail.com>
2025-03-06 06:50:50 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,7 +40,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~iJtT0QwJ#VjwullATAESoCqrb9RXoGA-lW55O1T9rs6P5huAnHMU",
 SUDO: process.env.SUDO || ["94717775628","94758775628"],
 PORT: process.env.PORT || "8000"
 };
